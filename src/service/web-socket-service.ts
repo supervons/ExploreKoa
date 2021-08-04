bf04c1643c7cfd2fabbf2faecf27b1dab6de6fd3
supervons <supervons@sina.com>
2021-08-04 16:51:42 +0800

    WebSocketService code optimization.

--- src/service/web-socket-service.ts
+++ src/service/web-socket-service.ts
@@ -5,14 +5,12 @@ import * as Koa from 'koa';
 let users = {};
 export default class WebSocketService {
   getTest = async (ctx: Koa.Context) => {
-    // 客户端链接传过来的客户端身份
-    let id: string = ctx.query.id + '';
-    // 将链接保存起来
+    let id: string = ctx.query.id as string;
+    // Save user websocket info.
     users[id] = ctx;
     ctx.websocket.send('连接成功');
-    // 监听客户端发送过来的信息
+    // Listen the client message.
     ctx.websocket.on('message', function (message) {
-      // uid为接收方，将接收到的信息发送给接收方uid,可以根据自己的需求处理数据再发送
       const uid = JSON.parse(message).uId;
       if (!users[uid]) {
         ctx.websocket.send(`${uid}未上线`);
