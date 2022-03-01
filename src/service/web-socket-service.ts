022c3eca5ced202c7edd5a16bdfd0e22dbbf1276
supervons <supervons@sina.com>
2022-03-01 14:55:13 +0800

    Change to en.

--- src/service/web-socket-service.ts
+++ src/service/web-socket-service.ts
@@ -8,12 +8,12 @@ export default class WebSocketService {
     let id: string = ctx.query.id as string;
     // Save user websocket info.
     users[id] = ctx;
-    ctx.websocket.send('连接成功');
+    ctx.websocket.send('Connection success!');
     // Listen the client message.
     ctx.websocket.on('message', function (message) {
       const uid = JSON.parse(message).uId;
       if (!users[uid]) {
-        ctx.websocket.send(`${uid}未上线`);
+        ctx.websocket.send(`${uid} offline!`);
       } else {
         users[uid].websocket.send(JSON.parse(message).content);
       }
